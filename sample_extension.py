7abdd6b2bf657cf883ef3ea020789acc71a041bf
Exenifix <yeligar@ya.ru>
2023-08-27 19:05:43 +0300

    Non-critical change

--- sample_extension.py
+++ sample_extension.py
@@ -10,7 +10,7 @@ class SampleCog(Cog):
         """
         Sample command
         """
-        await inter.send("Hi yo mom")
+        await inter.send("Hi")
 
     @tasks.loop(seconds=3, count=3)
     async def sample_loop(self):
