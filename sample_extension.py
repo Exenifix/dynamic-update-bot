0c763297b4b50034af8bfd17e88190b018fd4fa2
Exenifix <yeligar@ya.ru>
2023-08-27 18:08:12 +0300

    Test commit

--- sample_extension.py
+++ sample_extension.py
@@ -10,7 +10,7 @@ class SampleCog(Cog):
         """
         Sample command
         """
-        await inter.send("Hi")
+        await inter.send("Hi yo mama is fat")
 
     @tasks.loop(seconds=3, count=3)
     async def sample_loop(self):
