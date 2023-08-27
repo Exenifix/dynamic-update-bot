a73577f47678927ca19a024c4c298a8aa84de7e4
Exenifix <yeligar@ya.ru>
2023-08-27 19:09:48 +0300

    Supress curl output

--- .github/deploy/deploy.sh
+++ .github/deploy/deploy.sh
@@ -13,7 +13,7 @@ docker_restart() {
 
 update() {
   echo "Performing request to running bot..."
-  curl -s -X POST -H "Authorisation: $API_SECRET" http://localhost:3030/update
+  curl -s -X POST -H "Authorisation: $API_SECRET" http://localhost:3030/update > /dev/null
   echo "Request done"
 }
 
--- sample_extension.py
+++ sample_extension.py
@@ -10,7 +10,7 @@ class SampleCog(Cog):
         """
         Sample command
         """
-        await inter.send("Hi")
+        await inter.send(f"Hi {inter.user.mention}")
 
     @tasks.loop(seconds=3, count=3)
     async def sample_loop(self):
